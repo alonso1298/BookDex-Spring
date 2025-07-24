c2cfbadebb3e5cec94866f9f8addec91aad9eccd
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-24 05:41:56 -0600

    Add class Book

--- BookDex/src/main/java/com/alonsoSG/BookDex/model/Book.java
+++ BookDex/src/main/java/com/alonsoSG/BookDex/model/Book.java
@@ -3,4 +3,7 @@ package com.alonsoSG.BookDex.model;
 public class Book {
     private Long id;
     private String titulo;
+    private List<Autores> autores;
+    private String idioma;
+    private Integer numeroDeDescargas;
 }
