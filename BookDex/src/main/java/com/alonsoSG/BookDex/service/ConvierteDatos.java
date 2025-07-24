2d5632dbb5637b0b19600d129590971994fb4348
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-24 05:33:24 -0600

    Add Interface

--- BookDex/src/main/java/com/alonsoSG/BookDex/service/ConvierteDatos.java
+++ BookDex/src/main/java/com/alonsoSG/BookDex/service/ConvierteDatos.java
@@ -1,9 +1,19 @@
 package com.alonsoSG.BookDex.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConvierteDatos {
+public class ConvierteDatos implements IConvierteDatos{
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch(JsonProcessingException e) {
+            throw new RuntimeException();
+        }
+    }
+
+     
 }
