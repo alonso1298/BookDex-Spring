7b316e7ea99361143a74d54fb5486963f687d183
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-30 01:21:03 -0600

    Refactorizacion de la clase autor

--- BookDex/src/main/java/com/alonsoSG/BookDex/model/Autor.java
+++ BookDex/src/main/java/com/alonsoSG/BookDex/model/Autor.java
@@ -1,14 +1,10 @@
 package com.alonsoSG.BookDex.model;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,9 +17,6 @@ public class Autor {
     private String nombre;
     private Integer fechaDeNacimiento;
     private Integer fechaDeFallecimiento;
-
-    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
-    private List<Libro> libros;
 
     public Autor(){}
 
