f5aafb239c5f76c2627121e1354d2177fbd7aa02
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-30 02:29:14 -0600

    Refactor tablas

--- BookDex/src/main/java/com/alonsoSG/BookDex/model/Autor.java
+++ BookDex/src/main/java/com/alonsoSG/BookDex/model/Autor.java
@@ -1,10 +1,14 @@
 package com.alonsoSG.BookDex.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +21,8 @@ public class Autor {
     private String nombre;
     private Integer fechaDeNacimiento;
     private Integer fechaDeFallecimiento;
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
+    private List<Libro> libros;
 
     public Autor(){}
 
--- BookDex/src/main/java/com/alonsoSG/BookDex/model/Libro.java
+++ BookDex/src/main/java/com/alonsoSG/BookDex/model/Libro.java
@@ -2,14 +2,13 @@ package com.alonsoSG.BookDex.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,7 +19,7 @@ public class Libro {
     private Long id;
     @Column(unique = true)
     private String titulo;
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToOne
     private Autor autor;
     @ElementCollection
     private List<String> idiomas;
